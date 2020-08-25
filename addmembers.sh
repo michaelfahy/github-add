db49f8536810559718e28e96a82f632366e81e25
Michael Fahy <fahy@chapman.edu>
2020-08-25 11:28:14 -0700

    Use https instead of http

--- addmembers.sh
+++ addmembers.sh
@@ -1,17 +1,18 @@
 #! /usr/bin/bash
 # Name: addmembers.sh
 # AUthor: Michael Fahy
-# Version: 1.1
-# Date: 8/23/2020
+# Version: 1.2
+# Date: 8/25/2020
 
 read -r myusername< myGithubUsername
 read -r mypat < myPersonalAccessToken
 read -r myorg < myOrg
-githubapi="http://api.github.com/orgs/$myorg"
+githubapi="https://api.github.com/orgs/$myorg"
 
 while read -r line; do
    newmember="$line"
    command="curl -X PUT -u $myusername:$mypat $githubapi"
    command="$command""/memberships/$newmember"
-   eval "$command"
+   echo "$command"
+   #eval "$command"
 done < newMembers
--- addtoteam.sh
+++ addtoteam.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/bash
 # Name: addtoteam.sh
 # AUthor: Michael Fahy
-# Version: 1.1
-# Date: 8/23/2020
+# Version: 1.2
+# Date: 8/25/2020
 
 read -r myusername< myGithubUsername
 read -r mypat < myPersonalAccessToken
 read -r myorg < myOrg
 read -r myteam < myTeam
-githubapi="http://api.github.com/orgs/$myorg"
+githubapi="https://api.github.com/orgs/$myorg"
 
 while read -r line; do
    member="$line"
